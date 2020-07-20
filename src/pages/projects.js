4d053f804d46f0c58ece691bfe9f8d8e7d763fb5
MustyMouse <admin@krux.us>
2020-07-20 16:14:42 -0500

    Adding Cosmo to projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -7,6 +7,7 @@ export default ({ data }) => {
       <h1><span role="img" aria-label="Under Construction Emoji">🚧</span> Projects</h1>
       <p>This is where you'll find info on the various projects I've worked on over the years.</p>
       <li><a href="https://wordpress.org/plugins/multisite-rest-api/" target="_blank" rel="noreferrer">Multisite REST API</a></li>
+      <li><a href="https://top.gg/bot/538953009376526339" target="_blank" rel="noreferrer">Cosmo Discord Bot</a></li>
     </Layout>
   )
 }
