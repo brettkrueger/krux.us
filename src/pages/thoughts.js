fbac6ea0329806081323ef2c98cfe666c894b011
MustyMouse <admin@krux.us>
2020-07-20 17:41:37 -0500

    Updating info + order of posts

--- src/pages/thoughts.js
+++ src/pages/thoughts.js
@@ -9,7 +9,7 @@ export default ({ data }) => {
       <h3>A Bit About Me</h3>
       <p>I grew up in a small town you may have heard of called Austin. It's smack in the middle of Texas, so it's pretty hard to miss.
       I first got into computers around age 10 when I built my first gaming PC with my mom using Building a PC for Dummies. The early naughts were a simpler time.</p>
-      <p>As you can tell, I'm not a designer. This site is built using WordPress' REST API and Gatsby, and it uses the most basic template Gatsby comes with.
+      <p>As you can tell, I'm not a designer. This site is built using WordPress' REST API (served through GraphQL) and Gatsby, and it uses the most basic template Gatsby comes with.
       My skills are primarily focused on backend development. I work with languages such as PHP, Python, and NodeJS to accomplish my goals (BASH too, of course, but we don't talk about that).</p>
       <sub>I don't have a traditional blog page, so here's...</sub>
       <h3>My Thoughts</h3>
@@ -27,7 +27,7 @@ export default ({ data }) => {
 
 export const pageQuery = graphql`
   query {
-    allWpPost(sort: { fields: [date] }) {
+    allWpPost(sort: { fields: date, order: DESC }) {
       edges {
         node {
           title
