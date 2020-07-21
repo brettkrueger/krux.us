a76f3715a0881d0f2d2f6434487cc6e708d14211
MustyMouse <admin@krux.us>
2020-07-21 00:42:10 -0500

    Fix dat favicon.ico

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import React from "react"
 //import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import favicon from "../assets/images/favicon.ico"
 
 export default function Home({ data }) {
   return (
     <Layout>
+      <link rel="icon" href={favicon} />
       <SEO title="Home"/>
       <h1><span role="img" aria-label="Waving Hand Emoji">👋</span> Greetings!</h1>
       <p>This is my site. It's not much, but it's mine. Make yourself comfortable! Check out my projects and my "blog" (if you can really call it that), should it strike your interest.</p>
